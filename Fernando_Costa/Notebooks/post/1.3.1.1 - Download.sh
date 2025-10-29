3d231442eb6f12b3c2629089c76dffed7e8fa6a9
OscarFraile <estemailesparaspam@gmail.com>
2025-10-29 07:53:31 +0100

    Actualizar notebooks pre/post de Fernando

---
+++ Fernando_Costa/Notebooks/post/1.3.1.1 - Download.sh
@@ -0,0 +1,6 @@
+curl https://gist.githubusercontent.com/RobVanGroenewoud/ba89ad7684df8cefe5c183adb498cc65/raw/f2eec6d2cb89f5d779e16b28ed0dab89d738ba96/sample.csv > ejemplo.csv
+mkdir data2
+cd data2
+mv ../ejemplo.csv ejemplo.csv
+cat ejemplo.csv
+open ejemplo.csv
